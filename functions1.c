df67be813f2535c97c5611d4b9a09454d7432a4e
Kaych3e <nkaduba@gmail.com>
2022-11-21 22:25:54 +0000

    Function one

--- functions1.c
+++ functions1.c
@@ -63,9 +63,10 @@ int flags, int width, int precision, int size)
 			buffer[i--] = (num % 8) + '0';
 			num /= 8;
 		}
-	if (flags & F_HASH && init_num != 0)
+	if
+		(flags & F_HASH && init_num != 0)
 		buffer[i--] = '0';
-		i++;
+	i++;
 		return (write_unsgnd(0, i, buffer, flags, width, precision, size));
 }
 
